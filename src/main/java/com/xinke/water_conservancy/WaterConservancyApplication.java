6e1c6ebe285eddf5840a997df882f56ad16631a9
bigmonkey521 <1004363192@qq.com>
2019-11-13 10:25:54 +0800

    景一-初始化项目

---
+++ src/main/java/com/xinke/water_conservancy/WaterConservancyApplication.java
@@ -0,0 +1,13 @@
+package com.xinke.water_conservancy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaterConservancyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WaterConservancyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xinke/water_conservancy/WaterConservancyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xinke.water_conservancy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaterConservancyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
